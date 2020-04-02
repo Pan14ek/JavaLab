a45e62f3273e09fc244a1cdb975f526edcb757b1
Oleksii Makieiev1 <oleksii_makieiev1@epam.com>
2020-04-02 12:17:00 +0300

    master | Added project

---
+++ src/main/java/ua/nure/makieiev/labs/JavaLabsApplication.java
@@ -0,0 +1,14 @@
+package ua.nure.makieiev.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/ua/nure/makieiev/labs/JavaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.makieiev.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
